91ef8c0720f39ed874b16887431b8bbc77cd23e2
Santiesteban <alvaro.alfaro@outlook.com>
2017-09-19 13:38:05 -0600

    Added support for CollectionView and TableView

---
+++ CoreDataTest/CoreDataMainViewController.swift
@@ -0,0 +1,119 @@
+//
+//  CoreDataMainViewController.swift
+//  CoreDataTest
+//
+//  Created by Alvaro Santiesteban on 9/19/17.
+//  Copyright © 2017 3vts. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataMainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: Properties
+    
+    var fetchedResultsController : NSFetchedResultsController<NSFetchRequestResult>? {
+        didSet {
+            // Whenever the frc changes, we execute the search and
+            // reload the collection
+            fetchedResultsController?.delegate = self
+            executeSearch()
+            collectionView?.reloadData()
+        }
+    }
+    
+    // MARK: Initializers
+    
+    // Do not worry about this initializer. I has to be implemented
+    // because of the way Swift interfaces with an Objective C
+    // protocol called NSArchiving. It's not relevant.
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+// MARK: - CoreDataMainViewController (Subclass Must Implement)
+
+extension CoreDataMainViewController {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        fatalError("This method MUST be implemented by a subclass of CoreDataTableViewController")
+    }
+    
+}
+
+// MARK: - CoreDataMainViewController (Collection Data Source)
+
+extension CoreDataMainViewController {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        if let fc = fetchedResultsController {
+            return (fc.sections?.count)!
+        } else {
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let fc = fetchedResultsController {
+            return fc.sections![section].numberOfObjects
+        } else {
+            return 0
+        }
+    }
+    
+}
+
+// MARK: - CoreDataMainViewController (Fetches)
+
+extension CoreDataMainViewController {
+    
+    func executeSearch() {
+        if let fc = fetchedResultsController {
+            do {
+                try fc.performFetch()
+            } catch let e as NSError {
+                print("Error while trying to perform a search: \n\(e)\n\(fetchedResultsController!)")
+            }
+        }
+    }
+}
+
+// MARK: - CoreDataMainViewController: NSFetchedResultsControllerDelegate
+
+extension CoreDataMainViewController: NSFetchedResultsControllerDelegate {
+    
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+        let set = IndexSet(integer: sectionIndex)
+        
+        switch (type) {
+        case .insert:
+            collectionView?.insertSections(set)
+        case .delete:
+            collectionView?.deleteSections(set)
+        default:
+            // irrelevant in our case
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch(type) {
+        case .insert:
+            collectionView?.insertItems(at: [newIndexPath!])
+        case .delete:
+            collectionView?.deleteItems(at: [indexPath!])
+        case .update:
+            collectionView?.reloadItems(at: [indexPath!])
+        case .move:
+            collectionView?.deleteItems(at: [indexPath!])
+            collectionView?.insertItems(at: [newIndexPath!])
+        }
+    }
+}
---
+++ CoreDataTest/MainSubclassViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MainSubclassViewController.swift
+//  CoreDataTest
+//
+//  Created by Alvaro Santiesteban on 9/19/17.
+//  Copyright © 2017 3vts. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+private let reuseIdentifier = "Cell2"
+
+class MainSubclassViewController: CoreDataMainViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let stack = delegate.stack
+        
+        //Create fetchrequest
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "TestEntity")
+        fr.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        //Create the FetchedResultsController
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fr, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
+    }
+
+    @IBAction func addNewTest(_ sender: UIBarButtonItem) {
+        let image = UIImagePNGRepresentation(UIImage(named: "Image")!)! as NSData
+        
+        let test = TestEntity(title: "Title", subtitle: "Subtitle", image: image, context: (fetchedResultsController?.managedObjectContext)!)
+        
+        print("Just created a test\(test)")
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let test = fetchedResultsController!.object(at: indexPath) as! TestEntity
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TestCollectionViewCell
+        cell.imageView.image = UIImage(data: test.image! as Data)
+        
+        return cell
+    }
+
+}
